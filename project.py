ea5069c75b0b415531705e39f1cb6e38beaeedfb
BRIAN-cloud-cyber <omondiomimo@gmail.com>
2025-06-13 17:33:28 +0300

    updated project.py

--- project.py
+++ project.py
@@ -20,23 +20,24 @@ def search_word(word):
     
     if response.status_code==200:
         data=response.json()
-        meanings=data[0]("meanings")
-        definitions=meanings[0] ["definitions"]
+        meanings=data[0]("meanings")  #gets the first dictionary in the list[0] ,(meanings) get the value associated with the meanings key
+        definitions=meanings[0] ["definitions"] #meanings[0] take the first meaning [defitions]gets the lists of the definitions
         return definitions[0] ["definitions"]
-
+# take the first word result,the first part of speech(e.g noun),the definition,and return the text  of that definition 
     elif response.status_code==404:
         return "No definition found"
     else:
         return "Error while fetching the defition "
 
-def add_word(word,word_id):
-    if not verify_api_key():
+
+def add_word(word,word_id): # should enter the such word and the word_id
+    if not verify_api_key():# verifys if the key is correct
         return
     definition=search_word(word)
     entry={"id":word_id,   "word":word,"definitions":definition}
     
 
-    saved_words.append(entry)
+    saved_words.append(entry) # adds the suched word to the list 
     print(f"'{word}'saved successfully" )
 
 
@@ -46,16 +47,16 @@ def view_words():
     if not saved_words():
         print("No saved words.")
         return
-    for entry in saved_words:
+    for entry in saved_words: # loops through 
         print(f"\n ID:{entry['id']}\n word:{entry['word']}\n definition:{definition}")
 
 
 
-def update_word(word_id,new_word):
+def update_word(word_id,new_word): #find a word using its id 
     if not verify_api_key():
         return
     for entry in saved_words():
-        if entry["id"]==word_id:
+        if entry["id"]==word_id: #confirms for the word_id if present
             entry["word"]=new_word
             entry["definition"]=search_word(new_word)
             print(f"Word ID {word_id} updated")
@@ -63,17 +64,17 @@ def update_word(word_id,new_word):
     print("Word ID not found ")
 
 
-def delete_word(word_id):
+def delete_word(word_id): # Removes an entry with a specific id using list compression
     if not verify_api_key():
         return
     global saved_words
-    saved_words=[entry for entry in saved_words if entry["id"] !=word_id]
+    saved_words=[entry for entry in saved_words if entry["id"] !=word_id] # this craetes a new list that keeps every item except the one with the matching ID
     print(f"Word ID {word_id} deleted successfully")
 
 def clear_words():
     if not verify_api_key():
         return
-    saved_words.clear()
+    saved_words.clear()# removes everything from the list
     print("All words cleared.")
 
 
